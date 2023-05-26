8a37108883573a8e65fedcd47de50563123f00a0
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-05-26 08:01:19 -0700

    wip

--- pkg/models/search.go
+++ pkg/models/search.go
@@ -1,6 +1,7 @@
 package models
 
 type SearchResult struct {
+<<<<<<< Updated upstream
 	Message *Message               `json:"message"`
 	Summary *Summary               `json:"summary"`        // reserved for future use
 	Meta    map[string]interface{} `json:"meta,omitempty"` // reserved for future use
@@ -10,4 +11,15 @@ type SearchResult struct {
 type SearchPayload struct {
 	Text string                 `json:"text"`
 	Meta map[string]interface{} `json:"meta,omitempty"` // reserved for future use
+=======
+	Message  *Message               `json:"message"`
+	Summary  *Summary               `json:"summary"` // reserved for future use
+	Metadata map[string]interface{} `json:"metadata,omitempty"`
+	Dist     float64                `json:"dist"`
+}
+
+type SearchPayload struct {
+	Text     string                 `json:"text"`
+	Metadata map[string]interface{} `json:"metadata,omitempty"`
+>>>>>>> Stashed changes
 }
